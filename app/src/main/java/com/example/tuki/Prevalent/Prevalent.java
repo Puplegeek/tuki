9c86b9d2b50caf7231e0d8dd5315644b378d1c6f
syed akash <syedakashscu@outlook.com>
2022-04-30 14:11:45 +0600

    Initial commit - log in

---
+++ app/src/main/java/com/example/tuki/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.tuki.Prevalent;
+
+import com.example.tuki.Model.Users;
+
+public class Prevalent {
+
+    private static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
